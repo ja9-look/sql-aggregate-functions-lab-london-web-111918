49a49c74ccb63fabf3355e7d05749220e651c04a
Janine Luk <janineluk95@gmail.com>
2018-11-27 19:34:37 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
-  "SELECT name FROM students MAX"
+  "SELECT MAX("
 end
 
 def lowest_student_gpa
